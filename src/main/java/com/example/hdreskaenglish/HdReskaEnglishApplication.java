02ab4cee61264bb33fe1f9ced95a84a59f79178e
Olex Mashchenko <mitel4950@gmail.com>
2023-11-05 19:42:34 +0200

    Initial commit

---
+++ src/main/java/com/example/hdreskaenglish/HdReskaEnglishApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdreskaenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdReskaEnglishApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HdReskaEnglishApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/hdreskaenglish/HdReskaEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdreskaenglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdReskaEnglishApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
